a883eb116b08cc6ae7e85591a6077822d8fc0e02
othillo <othillo@users.noreply.github.com>
2023-06-19 17:46:56 +0000

    Apply coding standards

--- src/SensitiveData.php
+++ src/SensitiveData.php
@@ -15,22 +15,13 @@ namespace Broadway\BroadwaySensitiveData\EventHandling;
 
 class SensitiveData
 {
-    /**
-     * @var mixed
-     */
     private $data;
 
-    /**
-     * @param mixed $data
-     */
     public function __construct($data)
     {
         $this->data = $data;
     }
 
-    /**
-     * @return mixed
-     */
     public function getData()
     {
         return $this->data;
